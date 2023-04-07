a69ae5aa9f9de86fa091ff8a31dd372375ecefca
rotsyrotsy1 <rafa.rotsy@gmail.com>
2023-04-07 12:35:22 +0000

    Replace index.tsx

--- pages/404/index.tsx
+++ pages/404/index.tsx
@@ -1,14 +1,16 @@
 import { Typography, Link, Grid, IconButton } from '@mui/material'
 import React from 'react'
-import ImgNotFound from './Image/ImgNotFound'
 import Layout from './layout/Layout';
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import Home from "@mui/icons-material/Home";
 import Container from "@mui/material/Container";
 import { useRouter } from 'next/router';
+import Image from 'next/image';
+import useBasePath from '../../hooks/useBasePath';
 
 const PageNotFound = () => {
     const router = useRouter();
+    const basePath = useBasePath();
     return (
         <Layout>
             <Container  >
@@ -22,7 +24,7 @@ const PageNotFound = () => {
                     </Grid>
                     <Grid item xs={12} md={8} lg={8}>
                         <Typography variant="h4" color="initial" sx={{my: 2}}>Oops !</Typography>
-                            <ImgNotFound/>
+                            <Image src={`${basePath}/images/error/404.png`} alt='404' width={860} height={571} />
                         <Typography variant="h4" color="initial" sx={{my: 2}}>Cette page n&apos;existe pas encore !</Typography>
                     </Grid>
                     <Grid item xs={12} md={2} lg={2}>
