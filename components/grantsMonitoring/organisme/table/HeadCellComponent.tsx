584b07a5206e2a846a15c547fe816404dcafdb85
tsilefy12 <tsilefyandriamalaza@gmail.com>
2024-05-30 11:13:58 +0300

    correction SE

--- components/grantsMonitoring/organisme/table/HeadCellComponent.tsx
+++ components/grantsMonitoring/organisme/table/HeadCellComponent.tsx
@@ -4,63 +4,63 @@ import React, { useEffect } from "react";
 import { HeadCell } from "./HeadCell.interface";
 
 const HeadCellComponent = ({
-	headCell,
-	children,
+  headCell,
+  children,
 }: {
-	headCell: HeadCell;
-	[key: string]: any;
+  headCell: HeadCell;
+  [key: string]: any;
 }) => {
-	const [order, setOrder] = React.useState<any>("asc");
-	const [orderBy, setOrderBy] = React.useState<any>("");
-	const router = useRouter();
+  const [order, setOrder] = React.useState<any>("asc");
+  const [orderBy, setOrderBy] = React.useState<any>("");
+  const router = useRouter();
 
-	useEffect(() => {
-		// intialize order status from url
-		if (router.query.orderBy == headCell.id) {
-			setOrderBy(router.query.orderBy);
-			setOrder(router.query.order);
-		} else {
-			setOrderBy("");
-			setOrder("asc");
-		}
-	}, [router.query.orderBy]);
+  useEffect(() => {
+    // intialize order status from url
+    if (router.query.orderBy == headCell.id) {
+      setOrderBy(router.query.orderBy);
+      setOrder(router.query.order);
+    } else {
+      setOrderBy("");
+      setOrder("asc");
+    }
+  }, [router.query.orderBy]);
 
-	const handleClickOrder = (
-		event: React.MouseEvent<unknown>,
-		headCellId: string
-	) => {
-		let targetOrder = "asc";
-		if (orderBy == headCellId) {
-			targetOrder = order == "asc" ? "desc" : "asc";
-		} else {
-			targetOrder = "asc";
-		}
-		setOrder(targetOrder);
-		setOrderBy(headCellId);
-		router.push({
-			query: {
-				...router.query,
-				order: targetOrder,
-				orderBy: headCellId,
-			},
-		});
-	};
-	return (
-		<TableCell
-			key={headCell.id}
-			align={"center"}
-			padding={headCell.disablePadding ? "none" : "normal"}
-			sortDirection={orderBy === headCell.id ? order : false}
-		>
-			<TableSortLabel
-				active={orderBy === headCell.id}
-				direction={order}
-				onClick={(event) => handleClickOrder(event, headCell.id)}
-			>
-				{headCell.label}
-			</TableSortLabel>
-		</TableCell>
-	);
+  const handleClickOrder = (
+    event: React.MouseEvent<unknown>,
+    headCellId: string
+  ) => {
+    let targetOrder = "asc";
+    if (orderBy == headCellId) {
+      targetOrder = order == "asc" ? "desc" : "asc";
+    } else {
+      targetOrder = "asc";
+    }
+    setOrder(targetOrder);
+    setOrderBy(headCellId);
+    router.push({
+      query: {
+        ...router.query,
+        order: targetOrder,
+        orderBy: headCellId,
+      },
+    });
+  };
+  return (
+    <TableCell
+      key={headCell.id}
+      align={"center"}
+      padding={headCell.disablePadding ? "none" : "normal"}
+      sortDirection={orderBy === headCell.id ? order : false}
+    >
+      <TableSortLabel
+        active={orderBy === headCell.id}
+        direction={order}
+        onClick={(event) => handleClickOrder(event, headCell.id)}
+      >
+        {headCell.label}
+      </TableSortLabel>
+    </TableCell>
+  );
 };
 
 export default HeadCellComponent;
